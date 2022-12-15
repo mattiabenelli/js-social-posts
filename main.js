ba2618bb5154fd293e4651888f8048c36048caaf
mattiabenelli <benellimattia0@gmail.com>
2022-12-15 15:27:35 +0100

    milestone 2

--- main.js
+++ main.js
@@ -55,3 +55,42 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let itemsContent = "";
+posts.forEach((elem) => {
+    itemsContent += 
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${elem.author.image}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${elem.author.name}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${elem.content}</div>
+        <div class="post__image">
+            <img src="${elem.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${elem.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+})
+const itemsSlider = document.querySelector('.posts-list');
+itemsSlider.innerHTML += itemsContent;
